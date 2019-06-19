668a53fc406c16c42b66b70eb2076939b5c0c876
zajko04 <zajkoc6@gmail.com>
2019-06-19 22:51:50 +0200

    !ll

--- app/src/main/java/com/example/wikipedialivesearch/MainActivity.java
+++ app/src/main/java/com/example/wikipedialivesearch/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.wikipedialivesearch;
-
-import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.icu.text.UnicodeSetSpanner;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -19,7 +16,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -44,10 +40,11 @@ import java.util.stream.Collectors;
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
+
     SearchView searchView;
     Button buttons[];
     String links [] = new String[5];
-    String findings [] = new String[5];
+
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -56,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         setContentView(R.layout.activity_main);
-        searchView = (SearchView) findViewById(R.id.searchField);
+        searchView = findViewById(R.id.searchField);
         buttons = new Button[5];
         buttons[0] = findViewById(R.id.button1);
         buttons[1] = findViewById(R.id.button2);
@@ -65,8 +62,6 @@ public class MainActivity extends AppCompatActivity {
         buttons[4] = findViewById(R.id.button5);
 
 
-        final Find find = new Find();
-        find.start();
         for(int i=0; i<5; i++){
             final int finalI = i;
             buttons[i].setOnClickListener(new View.OnClickListener() {
@@ -79,9 +74,6 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
-
-
-
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
@@ -90,39 +82,51 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                find.setIn(newText);
-
-                while (find.getGo() == true){
-                    ;
+                try {
+                    go(newText);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
                 }
-                while (find.getIn() != newText){
-                    ;
-                }
-
-                links = find.getLinks();
-                findings = find.getFindings();
-
-                if(newText.length() != 0){
-                    for(int i=0 ;i<5; i++){
-                        buttons[i].setText(findings[i]);
-                        buttons[i].setVisibility(View.VISIBLE);
-                    }
-                }
-                else{
-                    for(int i=0 ;i<5; i++){
-                        buttons[i].setVisibility(View.INVISIBLE);
-                    }
-                }
-
                 return false;
             }
         });
 
     }
 
-    public void setLinks(String[] links) {
-        this.links = links;
+    private void go(String in) throws IOException, JSONException {
+        if(in.length() >0) {
+            String urls = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+in+"&limit=5&namespace=0&format=json";
+            URL ur1 = new URL(urls);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(ur1.openStream()));
+            String line;
+            String elo="";
+            while((line = reader.readLine()) != null){
+                elo +=line;
+            }
+
+            JSONArray jsonArray = new JSONArray(elo);
+            JSONArray searchResults = new JSONArray(jsonArray.get(1).toString());
+            JSONArray links = new JSONArray(jsonArray.get(3).toString());
+
+            for(int i=0; i<5; i++){
+                buttons[i].setText(searchResults.get(i).toString());
+                buttons[i].setVisibility(View.VISIBLE);
+                this.links[i] = links.get(i).toString();
+            }
+
+
+            reader.close();
+        }
+        else {
+            for(int i=0; i<5; i++){
+                buttons[i].setVisibility(View.INVISIBLE);
+            }
+        }
+
     }
+
+
+
 }
-
-
